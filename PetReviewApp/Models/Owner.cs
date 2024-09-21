8986082d599bf4cb49dbf95f4c2e7cd6566ccff8
Aleksandr Grigorenko <grigxia86@gmail.com>
2024-09-21 23:43:38 +0300

    Added models

---
+++ PetReviewApp/Models/Owner.cs
@@ -0,0 +1,9 @@
+﻿namespace PetReviewApp.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+        public string City { get; set; }
+    }
+}
